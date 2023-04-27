088792bbe7d7e0a4209b3800b945e23126414043
Matt Wachowski <matt.wachowski@gmail.com>
2023-04-27 10:49:30 -0700

    Added test for vsense.

---
+++ src/test/vsense/main.c
@@ -0,0 +1,93 @@
+#include "pico/stdlib.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "uart_console/console.h"
+#include "hardware/gpio.h"
+#include "hardware/adc.h"
+
+#define VSENSE_GPIO 26
+#define ADC_BASE_GPIO 26
+
+#define POLL_MS 1000
+
+uint16_t vdrop_mv = 0;
+uint16_t vmax_mv = 3300;
+uint8_t started = 1;
+
+static inline uint32_t uptime_ms() {
+  return to_ms_since_boot(get_absolute_time());
+}
+
+static uint8_t get_num(const char* name, const char* vstr, int min, int max, uint16_t* val) {
+  const int v = atoi(vstr);
+  if (v == 0 && strcmp(vstr, "0")) {
+    printf("Syntax error parsing %s\n", name);
+    return 0;
+  }
+
+  if (v < min) {
+    printf("%s is below the minimum of %d\n", name, min);
+    return 0;
+  }
+
+  if (v > max) {
+    printf("%s is above the maximum of %d\n", name, min);
+    return 0;
+  }
+
+  *val = (uint16_t)v;
+  return 1;
+}
+
+static void start_stop_cmd(uint8_t argc, char* argv[]) {
+  started = 1 - started;
+  printf(started ? "started\n" : "stopped\n");
+}
+
+static void vmax_mv_cmd(uint8_t argc, char* argv[]) {
+  get_num("vmax_mv", argv[0], 1000, 30000, &vmax_mv);
+}
+
+static void vdrop_mv_cmd(uint8_t argc, char* argv[]) {
+  get_num("vdrop_mv", argv[0], 0, 5000, &vmax_mv);
+}
+
+struct ConsoleCallback callbacks[] = {
+    {"s", "Start/Stop adc reads", 0, start_stop_cmd},
+    {"vmax_mv", "Sets maximum voltage (accounts for voltage division)\n", 1, vmax_mv_cmd},
+    {"vdrop_mv", "Sets voltage drop (accounts for inline diodes/etc)\n", 1, vdrop_mv_cmd},
+};
+
+static void init_adc(void) {
+  adc_init();
+  adc_gpio_init(VSENSE_GPIO);
+  adc_select_input(VSENSE_GPIO - ADC_BASE_GPIO);
+}
+
+static void read_adc(void) {
+  const uint16_t val = adc_read();
+  const uint32_t mv = ((uint32_t)val * vmax_mv / (1 << 12)) + vdrop_mv;
+  printf("val: %u, vmax_mv: %u, vdrop_mv: %d, mv: %d\n",
+      val, vmax_mv, vdrop_mv, mv);
+}
+
+// program entry point
+int main() {
+  struct ConsoleConfig cc;
+  uart_console_init(&cc, callbacks, 1, CONSOLE_VT102);
+  init_adc();
+
+  uint32_t next_read_ms = 0;
+
+  while (1) {
+    const uint32_t current_ms = uptime_ms();
+    if (started && (current_ms >= next_read_ms)) {
+      read_adc();
+      next_read_ms = current_ms + POLL_MS;
+    }
+    uart_console_poll(&cc, "> ");
+    sleep_ms(20);
+  }
+  return 0;
+}
