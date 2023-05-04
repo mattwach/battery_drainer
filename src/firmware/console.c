6a15d774ee1d3ab5cb87a3dc1e37ad77c8e31c15
Matt Wachowski <matt.wachowski@gmail.com>
2023-05-03 18:52:35 -0700

    Added ical console command

--- src/firmware/console.c
+++ src/firmware/console.c
@@ -29,6 +29,47 @@ static uint8_t parse_int(const char* name, const char* vstr, int min, int max, i
   return 1;
 }
 
+static uint8_t is_float_zero(const char* vstr) {
+  int idx = 0;
+  uint8_t point_found = 0;
+  for (; *vstr; ++vstr, ++idx) {
+    switch (*vstr) {
+      case '0':
+        break;
+      case '.':
+        if (point_found) {
+          return 0;
+        }
+        point_found = 1;
+        break;
+      default:
+        return 0;
+    }
+  }
+  return 1;
+}
+
+static uint8_t parse_float(const char* name, const char* vstr, float min, float max, float* val) {
+  const float v = (float)atof(vstr);
+  if (v == 0.0 && !is_float_zero(vstr)) {
+    printf("Syntax error parsing %s\n", name);
+    return 0;
+  }
+
+  if (v < min) {
+    printf("%s is below the minimum of %f\n", name, min);
+    return 0;
+  }
+
+  if (v > max) {
+    printf("%s is above the maximum of %f\n", name, min);
+    return 0;
+  }
+
+  *val = v;
+  return 1;
+}
+
 static void discard_cmd(uint8_t argc, char* argv[]) {
   settings_load(settings);
 }
@@ -45,8 +86,8 @@ static void dump_global_settings(void) {
   const struct GlobalSettings* g = &(settings->global);
   printf("Global settings:\n");
   printf("  ical:                 %d milliOhms\n", g->ical_mohms);
-  printf("  vcal ratio:           %f\n", g->vcal_ratio);
-  printf("  finish display ratio: %f\n", g->finish_display_ratio);
+  printf("  vcal ratio:           %.2f\n", g->vcal_ratio);
+  printf("  finish display ratio: %.2f\n", g->finish_display_ratio);
   printf("  voltage sag:\n");
   printf("    interval:           %d seconds\n", g->vsag.interval_seconds);
   printf("    settle time:        %d ms\n", g->vsag.settle_ms);
@@ -55,9 +96,9 @@ static void dump_global_settings(void) {
   printf("    minimum temp:       %d C\n", g->fan.min_celsius);
   printf("    100% temp:          %d C\n", g->fan.max_celsius);
   printf("  FET settings:\n");
-  printf("    voltage slew:       %f seconds\n", g->slew.volts);
-  printf("    current slew:       %f seconds\n", g->slew.amps);
-  printf("    temp slew:          %f seconds\n", g->slew.celsius);
+  printf("    voltage slew:       %.2f seconds\n", g->slew.volts);
+  printf("    current slew:       %.2f seconds\n", g->slew.amps);
+  printf("    temp slew:          %.2f seconds\n", g->slew.celsius);
 }
 
 static void dump_profile(int i) {
@@ -66,7 +107,7 @@ static void dump_profile(int i) {
   printf("Profile %d:\n", i);
   printf("  name:             %s\n", p->name);
   printf("  vdrop:            %d mV\n", p->drop_mv);
-  printf("  max current:      %f A\n", (float)p->max_ma * 1000.0);
+  printf("  max current:      %.1f A\n", (float)p->max_ma / 1000.0);
   printf("  max temp:         %d C\n", p->max_celsius);
   printf("  max power:        %d Watts\n", p->max_watts);
   if (p->cell_count) {
@@ -97,8 +138,18 @@ static void show_cmd(uint8_t argc, char* argv[]) {
   }
 }
 
+static void ical_cmd(uint8_t argc, char* argv[]) {
+  float v = 0.0;
+  if (!parse_float("ical", argv[0], 0.01, 1.0, &v)) {
+    return;
+  }
+  settings->global.ical_mohms = (uint16_t)(v * 1000);
+  printf("ical changed to %d milliohms (not saved)\n", settings->global.ical_mohms);
+}
+
 struct ConsoleCallback callbacks[] = {
     {"discard", "Discard changes / reload flash", 0, discard_cmd},
+    {"ical", "Sets the current shunt resistance (ohms)", 1, ical_cmd},
     {"reset", "resets settings without saving.", 0, reset_cmd},
     {"save", "Write configuration to flash memory", 0, save_cmd},
     {"show", "Display settings", -1, show_cmd},
